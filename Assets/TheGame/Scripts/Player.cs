58040f99073ef0913537c8d3021ecd4567e0e2b3
Tristan78 <tristan78@gmx.de>
2020-12-19 02:25:11 +0100

    After unit 18. Laufbewegung programmieren

---
+++ Assets/TheGame/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Steuerung der Spielfigur.
+/// </summary>
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// Laufgeschwindigkeit der Figur.
+    /// </summary>
+    public float speed = 0.05f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position += Input.GetAxis("Horizontal") * speed * transform.forward;
+    }
+}
