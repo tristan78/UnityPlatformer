324f123a2abe1a6f8b080ef711c0c068d5522a49
Tristan78 <tristan78@gmx.de>
2020-12-22 23:59:46 +0100

    After unit 20. Drehen bei Richtungswechsel

--- Assets/TheGame/Scripts/Player.cs
+++ Assets/TheGame/Scripts/Player.cs
@@ -12,9 +12,28 @@ public class Player : MonoBehaviour
     /// </summary>
     public float speed = 0.05f;
 
+    /// <summary>
+    /// Das grafische Modell, u. a. für die Drehung in Laufrichtung.
+    /// </summary>
+    public GameObject model;
+
+    /// <summary>
+    /// Der Winkel zu dem sich die Figur um die eigene Achse (=Y) drehen soll.
+    /// </summary>
+    private float towardsY = 0f;
+
     // Update is called once per frame
     private void Update()
     {
-        transform.position += Input.GetAxis("Horizontal") * speed * transform.forward;
+        float h = Input.GetAxis("Horizontal");
+
+        transform.position += h * speed * transform.forward;
+
+        if (h > 0f) // nach rechts gehen
+            towardsY = 0f;
+        else if (h < 0f) // nach links gehen
+            towardsY = -180f;
+
+        model.transform.rotation = Quaternion.Lerp(model.transform.rotation, Quaternion.Euler(0f, towardsY, 0f), Time.deltaTime * 10f);
     }
 }
